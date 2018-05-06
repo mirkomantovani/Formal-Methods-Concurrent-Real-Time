79dc426aa36425ff5349f6cfe240541bf6e717c6
Alessio Sacco <alessiosacco95@gmail.com>
2018-05-06 14:10:48 +0200

    Add area modeling to document

---
+++ adj-predicates-latex.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+
+def pred(a, b):
+	"return the predicate of adjacency associated to the two areas"
+	# a, b are two indexes
+	area_a = "L{}".format(a)
+	area_b = "L{}".format(b)
+	return "Adj({}, {})".format(area_a, area_b)
+
+
+if __name__ == "__main__":
+	with open('adj-matrix.csv', 'r') as csvfile:
+		matrix = csv.reader(csvfile, delimiter=';')
+		output = ''
+		skip = True
+		for row in matrix:
+			if skip:
+				skip = False
+				continue
+			for i in range(1, len(row)):
+				column = i - 1
+				if row[i] == '0':
+					output += " \\land \\neg {}".format(pred(row[0], column))	
+				elif row[i] == '1':
+					output += " \\land {}".format(pred(row[0], column))	
+		print("{}".format(output))
