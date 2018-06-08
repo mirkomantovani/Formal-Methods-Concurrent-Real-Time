08febec8bdb3dd04efeb71a6dee6bc2337e40968
Alessio Sacco <alessiosacco95@gmail.com>
2018-06-08 18:10:18 +0200

    Prettier history

--- zot/clean-history.py
+++ zot/clean-history.py
@@ -1,27 +1,60 @@
 
 # coding: utf-8
 
-# In[13]:
+# In[80]:
 
 
 import sys
 
 
-# In[14]:
+# In[81]:
 
 
-hist = open('output.hist.txt', 'r')
+hist = open('../output.hist.txt', 'r')
 lines = hist.readlines()
 
 
-# In[15]:
+# In[82]:
 
 
-filter_adj = filter((lambda line: not ('ADJACENT' in line or 'DANGER' in line)), lines)
+def filter_lines(line):
+    is_adjacent = 'ADJACENT' in line
+    is_danger = "DANGER" in line
+    return not (is_adjacent or is_danger)
 
-# In[16]:
+
+# In[83]:
 
 
-for line in filter_adj:
+def split_time_instants(lines):
+    result = []
+    instant = []
+    for line in lines:
+        if "-----" in line:
+            result.append(instant)
+            instant = []
+        instant.append(line)
+    return result
+
+
+# In[84]:
+
+
+def order_time_instant(instant):
+    result = []
+    result.append(instant[0])
+    result += sorted(instant[1:])
+    return result
+
+
+# In[85]:
+
+
+filter_adj = list(filter(filter_lines, lines))
+instants = split_time_instants(filter_adj)
+result = []
+for instant in instants[1:]:
+    result += order_time_instant(instant)
+for line in result:
     sys.stdout.write(line)
 
