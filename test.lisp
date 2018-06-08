bdba4265019144bae0f2b6829d1bc7fb2135a8a3
Alessio Sacco <alessiosacco95@gmail.com>
2018-06-08 17:26:39 +0200

    Modify test.lisp

--- test.lisp
+++ test.lisp
@@ -1,6 +1,5 @@
 (asdf:operate 'asdf:load-op 'ae2zot)
-(use-package
-    :trio-utils)
+(use-package :trio-utils)
 
 (load "zot/grid.lisp")
 (load "zot/operator.lisp")
@@ -9,8 +8,52 @@
 (load "zot/ensured-safety-properties.lisp")
 (load "zot/robot-controller.lisp")
 
-(defconstant *time* 5)
+(defconstant test-1
+  (somf (&& (-V- pick)
+            (action-is pick-from-bin-area))))
 
+(defconstant test-2
+  (somf (&& (-V- holding)
+            (action-is pick-from-bin-area))))
+
+(defconstant test-3
+  (somf (&& (-V- holding)
+            (action-is drop-to-local-bin))))
+
+(defconstant test-4
+  (somf (&& (-V- drop)
+            (action-is drop-to-local-bin))))
+
+(defconstant test-5
+  (somf (&& (-V- pick)
+            (action-is pick-from-local-bin))))
+
+(defconstant test-6
+  (somf (&& (-V- holding)
+            (action-is pick-from-local-bin))))
+
+(defconstant test-7
+  (somf (&& (-V- holding)
+            (action-is drop-to-tombstone))))
+
+(defconstant test-8
+  (somf (&& (-V- drop)
+            (action-is drop-to-tombstone))))
+
+(defconstant unsat-1
+  (somf (&& (-V- drop)
+            (-E- x action-set (&& (!! ([=] (-V- current-action) drop-to-tombstone))
+                                  (!! ([=] (-V- current-action) drop-to-local-bin))
+                                  (action-is x))))))
+
+(defconstant unsat-2
+  (somf (&& (-V- pick)
+            (-E- x action-set (&& (!! ([=] (-V- current-action) pick-from-bin-area))
+                                  (!! ([=] (-V- current-action) pick-from-local-bin))
+                                  (action-is x))))))
+
+
+(defconstant *time* 20)
 (ae2zot:zot *time* (&& grid-axioms
                        cart-axioms
                        arm-axioms
@@ -23,6 +66,15 @@
                        ;; (!! no-dangerous-contact-operator-cart)
                        ;; (!! no-dangerous-contact-operator-cart-transit)
                        ;; (!! cart-does-not-move-into-operators-area)
-                       (yesterday (action-is go-to-bin-area))
-                       (somf (action-is pick-from-bin-area))
+                       test-1
+                       test-2
+                       test-3
+                       test-4
+                       test-5
+                       test-6
+                       test-7
+                       test-8
+
+                       ;; unsat-1
+                       ;; unsat-2
                        ))
